c457a301feac6c84b48f26eee1c532fb77067913
TomClancy-py <tomclancy5544@gmail.com>
2020-11-18 07:01:11 +0000

    New file added

---
+++ Main.java
@@ -0,0 +1,8 @@
+// this programme will tell your current time in your timezone
+import java.util.Date;
+public class Main{
+  public static void main(String[]args){
+  Date date = new Date();
+  System.out.println(date);
+  }
+}
